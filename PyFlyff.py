7d270361490110597c2d8af91f438527ea2f6538
Igor Lima <igorlimaesilva94@gmail.com>
2022-05-23 14:22:40 -0300

    Initial commit

---
+++ PyFlyff.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+from PyQt5.QtGui import QIcon
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.browser = QWebEngineView()
+        self.browser.setUrl(QUrl("https://universe.flyff.com/play"))
+        self.setCentralWidget(self.browser)
+        self.setWindowIcon(QIcon("icons/flyffu.ico"))
+        self.showMaximized()
+
+
+app = QApplication(sys.argv)
+
+QApplication.setApplicationName("PyFlyff")
+
+window = MainWindow()
+
+app.exec_()
