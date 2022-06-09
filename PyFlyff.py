c40c559f42a990405213e2228396e02f83d7df54
Igor Lima <igorlimaesilva94@gmail.com>
2022-06-09 10:43:06 -0300

    removed unnecessary import

--- PyFlyff.py
+++ PyFlyff.py
@@ -2,7 +2,6 @@ import json
 import pathlib
 import sys
 import time
-import tkinter
 
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QShortcut, QToolBar, QAction
